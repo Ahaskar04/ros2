fad1292770b8eb11d03d4bf9738b10715c0e2059
Ahaskar04 <kashyapahaskar@gmail.com>
2025-12-15 16:43:41 +0000

    OOP framework

--- my_py_pkg/my_py_pkg/my_first_node.py
+++ my_py_pkg/my_py_pkg/my_first_node.py
@@ -2,11 +2,16 @@
 import rclpy #python library for ros 2
 from rclpy.node import Node
 
+class MyNode(Node): #passes the main Node class
+
+    def __init__(self):
+        super().__init__("py_test") #creates a Node
+        self.get_logger().info("Hello World")
+
 
 def main(args=None):
     rclpy.init(args=args) #initialize ros 2 communication
-    node = Node("py_test")
-    node.get_logger().info("Hello World")
+    node = MyNode() #creating the node using our own class MyNode
     rclpy.spin(node) #keeps the node alive
     rclpy.shutdown() # close ros 2 communication
 
