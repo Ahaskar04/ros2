3d1ac830e81e59e16d3d6f49235c938bb6dfe1d7
Ahaskar04 <kashyapahaskar@gmail.com>
2025-12-15 16:55:30 +0000

    timer

--- my_py_pkg/my_py_pkg/my_first_node.py
+++ my_py_pkg/my_py_pkg/my_first_node.py
@@ -5,9 +5,14 @@ from rclpy.node import Node
 class MyNode(Node): #passes the main Node class
 
     def __init__(self):
-        super().__init__("py_test") #creates a Node
+        super().__init__("py_test") #creates a Node named py_test
+        self.counter_ = 0 #counter is in the class, can be used in any other functions of the class
         self.get_logger().info("Hello World")
+        self.create_timer(1.0, self.timer_callback) #we do not want to call the function, we're just registering the function
 
+    def timer_callback(self):
+        self.get_logger().info("Hello" + str(self.counter_))
+        self.counter_ += 1
 
 def main(args=None):
     rclpy.init(args=args) #initialize ros 2 communication
